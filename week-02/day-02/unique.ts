97a4ee8b3a9dfd0d060f7ce13b9fbae759cc8e05
morvaygergo <morvaygergo@gmail.com>
2018-10-30 17:18:44 +0100

    unique.ts

---
+++ week-02/day-02/unique.ts
@@ -0,0 +1,15 @@
+'use strict';
+export{};
+
+function unique(arr) {
+    let length: number = arr.length;
+    let sorted: number[] = arr.sort();
+    for (let i: number = 0; i < length; i++) {
+        if (sorted[i] == sorted[i+1]) {
+            sorted.splice(i, 1);
+        };
+    };
+    return sorted;
+};
+
+console.log(unique([1, 11, 34, 11, 52, 61, 1, 34]));
