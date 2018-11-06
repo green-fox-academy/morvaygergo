027ebff0a6f9e1162dd710a98890a7ba7b69fc1c
morvaygergo <morvaygergo@gmail.com>
2018-11-06 14:06:56 +0100

    solar-system.ts

---
+++ week-03/day-02/matchmaking.ts
@@ -0,0 +1,7 @@
+'use strict';
+export { };
+
+let girls: string[] = ['Eve', 'Ashley', 'Claire', 'Kat', 'Jane'];
+let boys: string[] = ['Joe', 'Fred', 'Tom', 'Todd', 'Neef', 'Jeff'];
+
+console.log(makingMatches(girls, boys));
---
+++ week-03/day-02/solar-system.ts
@@ -0,0 +1,11 @@
+'use strict';
+export { };
+
+let planetList: string[] = ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Uranus', 'Neptune'];
+
+function putSaturn(arr: string[]): string[] {
+  arr.splice(5,0,"Saturn");
+  return arr;
+};
+
+console.log(putSaturn(planetList));
