931aa67c1ea701619c8749b3f1b6748e4853968c
morvaygergo <morvaygergo@gmail.com>
2018-11-06 11:58:22 +0100

    takes-longer.ts

---
+++ week-03/day-02/takes-longer.ts
@@ -0,0 +1,10 @@
+'use strict';
+export { };
+
+let quote: string = 'Hofstadter\'s Law: It you expect, even when you take into account Hofstadter\'s Law.'
+
+function fixer (str: string): string {
+  return quote.substring(0,21) + "always takes longer than " + quote.substring(21,);
+};
+
+console.log(fixer(quote));
