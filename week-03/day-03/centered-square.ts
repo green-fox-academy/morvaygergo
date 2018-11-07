86ffb230b2ba533be85aebd7328f3c66436f1f94
morvaygergo <morvaygergo@gmail.com>
2018-11-07 11:47:37 +0100

    four-rectangles.ts

--- week-03/day-03/centered-square.ts
+++ week-03/day-03/centered-square.ts
@@ -4,4 +4,4 @@ const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
 const ctx = canvas.getContext('2d');
 
 ctx.fillStyle = "green";
-ctx.fillRect(((canvas.width/2)-5), ((canvas.height/2)-5), 10, 10);
+ctx.fillRect(((canvas.width / 2) - 5), ((canvas.height / 2) - 5), 10, 10);
