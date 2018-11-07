fae6318300e76a27ec87903a876df2f6a3ad2749
morvaygergo <morvaygergo@gmail.com>
2018-11-07 11:39:53 +0100

    centered-square.ts

---
+++ week-03/day-03/centered-square.ts
@@ -0,0 +1,7 @@
+'use strict';
+
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+
+ctx.fillStyle = "green";
+ctx.fillRect(((canvas.width/2)-5), ((canvas.height/2)-5), 10, 10);
