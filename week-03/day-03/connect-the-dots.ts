5d9aad6a9716dbd3899fab30eda19604f40f5eaf
morvaygergo <morvaygergo@gmail.com>
2018-11-07 15:17:23 +0100

    connect-the-dots.ts

--- week-03/day-03/connect-the-dots.ts
+++ week-03/day-03/connect-the-dots.ts
@@ -3,4 +3,15 @@
 const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
 const ctx = canvas.getContext('2d');
 
-let coordinates: number[] = []
+let coordinates: number[][] = [[50, 100], [70, 70], [80, 90], [90, 90], [100, 70], [120, 100], [85, 130], [50, 100]];
+
+function dotConnect(crd: number[][]) {
+  for (let i = 0; i < crd.length; i++) {
+    ctx.beginPath();
+    ctx.moveTo(crd[i][0], crd[i][1]);
+    ctx.lineTo(crd[i + 1][0], crd[i + 1][1]);
+    ctx.stroke();
+  };
+};
+
+dotConnect(coordinates);
