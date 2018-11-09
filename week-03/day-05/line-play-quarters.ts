53abec862d9433167a0536ee9217f3c1ed062331
morvaygergo <morvaygergo@gmail.com>
2018-11-09 10:38:35 +0100

    line-play.ts modified

--- week-03/day-05/line-play-quarters.ts
+++ week-03/day-05/line-play-quarters.ts
@@ -4,20 +4,20 @@ const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
 const ctx = canvas.getContext('2d');
 
 function linePlay1(color: string, i: number, max: number) {
-  ctx.strokeStyle = color;
-  ctx.beginPath();
-  ctx.moveTo(0, i);
-  ctx.lineTo(i + 10, max);
-  ctx.stroke();
+    ctx.strokeStyle = color;
+    ctx.beginPath();
+    ctx.moveTo(0, i);
+    ctx.lineTo(i + 10, max);
+    ctx.stroke();
 };
 
 
 function linePlay2(color: string, i: number, max: number) {
-  ctx.strokeStyle = color;
-  ctx.beginPath();
-  ctx.moveTo(i, 0);
-  ctx.lineTo(400, i + 10);
-  ctx.stroke();
+    ctx.strokeStyle = color;
+    ctx.beginPath();
+    ctx.moveTo(i, 0);
+    ctx.lineTo(400, i + 10);
+    ctx.stroke();
 };
 
 for (let i = 0; i <= 400; i += 10) {
