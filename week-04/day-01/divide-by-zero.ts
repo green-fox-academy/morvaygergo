c53418727e5765a68b34da39a9817b04131cba6f
morvaygergo <morvaygergo@gmail.com>
2018-11-12 12:01:38 +0100

    divide-by-zero.ts

---
+++ week-04/day-01/divide-by-zero.ts
@@ -0,0 +1,13 @@
+function divideTen(n: number): number {
+  if (n === 0) {
+    throw new TypeError('fail')
+  };
+  return (10 / n);
+};
+
+try {
+  console.log(divideTen(100));
+  console.log(divideTen(0));
+} catch (e) {
+  console.log(e.message);
+};
