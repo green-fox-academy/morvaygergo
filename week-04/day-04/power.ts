a1dae515201377af87beb7b22564736e4811c0c5
morvaygergo <morvaygergo@gmail.com>
2018-11-15 12:28:36 +0100

    power.ts

---
+++ week-04/day-04/power.ts
@@ -0,0 +1,12 @@
+'use strict';
+export { };
+
+function power(n: number, m: number): number {
+  if (m <= 1) {
+    return n
+  } else {
+    return n * power(n, m - 1)
+  };
+};
+
+console.log(power(6, 10));
