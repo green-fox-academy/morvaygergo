061222657ddc97aea51538963fb2e90c9284a00b
morvaygergo <morvaygergo@gmail.com>
2018-11-15 14:11:29 +0100

    string2.ts modified

--- week-04/day-04/string2.ts
+++ week-04/day-04/string2.ts
@@ -1,14 +1,14 @@
 'use strict';
 export { };
 
-function fromXToY(str: string): string {
+function xDeleter(str: string): string {
   if (str.length === 1 && str !== 'x') {
     return str;
   } else if (str[0] === 'x') {
-    return fromXToY(str.substring(1, str.length));
+    return xDeleter(str.substring(1, str.length));
   } else {
-    return str[0] + fromXToY(str.substring(1, str.length));
+    return str[0] + xDeleter(str.substring(1, str.length));
   };
 };
 
-console.log(fromXToY('xaxaxaxaxa'));
+console.log(xDeleter('xaxaxaxaxa'));
