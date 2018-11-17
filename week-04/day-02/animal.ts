23814b0fe6d0eb00354225c0136bb32a6c97863d
morvaygergo <morvaygergo@gmail.com>
2018-11-17 14:42:57 +0100

    animal.ts

--- week-04/day-02/animal.ts
+++ week-04/day-02/animal.ts
@@ -1,3 +1,21 @@
 'use strict';
 export { };
 
+class Animal {
+
+  hunger: number;
+  thirst: number;
+
+  eat(): void {
+    this.hunger -= 1;
+  };
+
+  drink(): void {
+    this.thirst -= 1;
+  };
+
+  play(): void {
+    this.hunger += 1;
+    this.thirst += 1;
+  };
+};
