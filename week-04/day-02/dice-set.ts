327506724c28e723a0aeda432d3e918c69a303fa
morvaygergo <morvaygergo@gmail.com>
2018-11-18 16:43:49 +0100

    dice-set.ts

---
+++ week-04/day-02/dice-set.ts
@@ -0,0 +1,55 @@
+class DiceSet {
+  dice: number[];
+  readonly numOfDices: number = 6;
+
+  roll(): number[] {
+    this.dice = [];
+    for (var i = 0; i < this.numOfDices; i++) {
+      this.dice.push(Math.floor(Math.random() * 6 + 1));
+    }
+    return this.dice;
+  }
+
+  reroll(index?: number) {
+    if (index == undefined) {
+      for (var i = 0; i < this.numOfDices; i++) {
+        this.dice[i] = Math.floor(Math.random() * 6 + 1);
+      }
+    } else {
+      this.dice[index] = Math.floor(Math.random() * 6 + 1);
+    }
+  }
+
+  getCurrent(index?: number) {
+    if (index == undefined) {
+      for (var i = 0; i < this.numOfDices; i++) {
+        return this.dice[i];
+      }
+    } else {
+      return this.dice[index];
+    }
+  }
+}
+
+// You have a `DiceSet` class which has a list for 6 dice
+// You can roll all of them with roll()
+// Check the current rolled numbers with getCurrent()
+// You can reroll with reroll()
+// Your task is to roll the dice until all of the dice are 6
+
+let diceSet = new DiceSet();
+
+function notSix(n: number): boolean {
+  return n !== 6;
+}
+
+diceSet.roll();
+
+while (diceSet.dice.some(notSix)) {
+  diceSet.dice.forEach(function (value: number, index: number, array: number[]): void {
+    if (value !== 6) {
+      diceSet.reroll(index);
+    }
+  });
+  console.log(diceSet.dice);
+}
