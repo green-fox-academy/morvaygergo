79d7af9a6c62c5650d0d521ba44397961beb43ef
morvaygergo <morvaygergo@gmail.com>
2018-11-19 12:26:09 +0100

    tree class created

---
+++ week-05/day-01/garden/tree.ts
@@ -0,0 +1,16 @@
+import { Plant } from "./plant";
+
+'use strict';
+
+export class Tree extends Plant {
+
+  constructor(color: string) {
+    super(color);
+  }
+
+  getWatered(water: number) {
+    super.getWatered(water);
+    this.waterAmount += water * 0.4;
+  }
+
+}
