c5cf5fc6a40eb0e0bee4faed82a44984a4bd1511
morvaygergo <morvaygergo@gmail.com>
2018-11-19 15:41:56 +0100

    aircraft methods created

--- week-05/day-01/aircraft-carrier/aircraft.ts
+++ week-05/day-01/aircraft-carrier/aircraft.ts
@@ -12,5 +12,27 @@ export class Aircraft {
     this.baseDamage = 0;
   }
 
+  fight(): number {
+    let damage: number = this.currentAmmo * this.baseDamage;
+    this.currentAmmo = 0;
+    return damage;
+  }
 
+  refill(ammo: number): number {
+    let need: number = this.maxAmmo - this.currentAmmo;
+    this.currentAmmo = this.maxAmmo;
+    return ammo - need;
+  }
+
+  getType(): string {
+    return typeof this;
+  }
+
+  getStatus(): string {
+    return `Type ${this.getType()}, Ammo: ${this.currentAmmo}, Base Damage: ${this.baseDamage}, All Damage: ${this.baseDamage * this.maxAmmo}`
+  }
+
+  isPriority(): boolean {
+    return this.getType() === 'F35';
+  }
 }
