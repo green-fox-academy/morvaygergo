1031a0863841656a1f744597aa5439ceb1bee0c5
morvaygergo <morvaygergo@gmail.com>
2018-11-21 12:17:51 +0100

    Reptile class created

--- week-05/day-03/zoo/reptile.ts
+++ week-05/day-03/zoo/reptile.ts
@@ -2,4 +2,25 @@
 
 import { Animal } from './animal';
 
-export class
+export class Reptile extends Animal {
+  protected extinct: boolean;
+
+  constructor(name: string, age: number, gender: string, size: string, indigenousTo: string, extinct: boolean) {
+    super(name, age, gender, size, indigenousTo);
+    this.extinct = extinct;
+  }
+
+  breed(): string {
+    return 'eggs'
+  }
+
+  feed(): string {
+    return 'sometimes plants but mostly meat'
+  }
+
+  isDino(): void {
+    if (this.extinct) {
+      console.log('I am a dinosaur!')
+    }
+  }
+}
