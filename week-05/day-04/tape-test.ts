b451aaeb3d4c4cc45f398b5f2fc5a24b035208eb
morvaygergo <morvaygergo@gmail.com>
2018-11-22 11:07:08 +0100

    tape trials

---
+++ week-05/day-04/tape-test.ts
@@ -0,0 +1,22 @@
+export function addNum (a: number, b: number): number {
+  return a + b;
+}
+
+export function subNum (a: number, b: number): number {
+  return a - b;
+}
+
+import { test } from 'tape';
+
+test('add 2 numbers', t => {
+  const actual = addNum(1, 2);
+  const expected = 3;
+
+  t.equal(actual, expected);
+  t.end();
+});
+
+test('test subNum()', t => {
+  t.equal(subNum(2, 1), 1);
+  t.end();
+})
