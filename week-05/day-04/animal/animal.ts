f2bfafbdc266c311993c983d97ea12baa588b68d
morvaygergo <morvaygergo@gmail.com>
2018-11-22 17:11:38 +0100

    Animal class created

---
+++ week-05/day-04/animal/animal.ts
@@ -0,0 +1,26 @@
+'use strict';
+export { Animal };
+
+class Animal {
+
+  hunger: number;
+  thirst: number;
+
+  constructor() {
+    this.hunger = 50;
+    this.thirst = 50;
+  };
+
+  eat(): void {
+    this.hunger -= 1;
+  };
+
+  drink(): void {
+    this.thirst -= 1;
+  };
+
+  play(): void {
+    this.hunger += 1;
+    this.thirst += 1;
+  };
+};
