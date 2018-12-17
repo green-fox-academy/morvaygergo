106e148f037b11dc5f37af3f91bd757e59e2c59c
morvaygergo <morvaygergo@gmail.com>
2018-12-17 14:33:08 +0100

    todo list

---
+++ w09-Backend/d01-HTTPServer_Templating/todo/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+const todos = [
+  'get up',
+  'survive',
+  'go back to bed',
+];
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('home', {
+    title: 'To Do List',
+    todos: todos
+  });
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
