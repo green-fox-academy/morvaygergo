57982955dad992ede78109525a9aa6b5d0d6ca93
morvaygergo <morvaygergo@gmail.com>
2018-12-19 16:34:53 +0100

    array handler

--- w09-Backend/d03-REST/project/app.js
+++ w09-Backend/d03-REST/project/app.js
@@ -4,6 +4,53 @@ const path = require('path');
 const app = express();
 const PORT = 8080;
 
+let untilSum = n => {
+  if (n === 1) {
+    return 1;
+  } else {
+    return n + untilSum(n - 1);
+  }
+}
+
+let untilFact = n => {
+  if (n === 1) {
+    return 1;
+  } else {
+    return n * untilFact(n - 1);
+  }
+}
+
+let arraySum = array => {
+  let n = 0;
+
+  for (let i = 0; i < array.length; i++) {
+    n += array[i];
+  }
+
+  return n;
+}
+
+let arrayMultiply = array => {
+  let n = 1;
+
+  for (let i = 0; i < array.length; i++) {
+    n *= array[i];
+  }
+
+  return n;
+}
+
+let arrayDouble = array => {
+  let a = [];
+
+  for (let i = 0; i < array.length; i++) {
+    a.push(array[i] * 2);
+  }
+
+  return a;
+}
+
+app.use(express.json());
 app.use('/assets', express.static('assets'));
 
 app.get('/', (req, res) => {
@@ -43,13 +90,52 @@ app.get('/greeter', (req, res) => {
   }
 });
 
-app.get('/:append', (req, res) => {
+app.get('/appenda/:append', (req, res) => {
   if (req.params.append) {
     res.json({
-     "appended": req.params.append + "a"
+      'appended': req.params.append.concat('a')
     });
   } else {
-    send.status(404);
+    res.status(404).send;
+  }
+});
+
+app.post('/dountil/:action', (req, res) => {
+  if (req.params.action === 'sum') {
+    res.json({
+      "result": untilSum(req.body.until)
+    });
+  } else if (req.params.action === 'factor') {
+    res.json({
+      "result": untilFact(req.body.until)
+    });
+  } else {
+    res.json({
+      "error": "Please provide a number!"
+    });
+  }
+});
+
+app.post('/arrays', (req, res) => {
+  const { what } = req.body;
+  const { numbers } = req.body;
+
+  if (what === 'sum') {
+    res.json({
+      "result": arraySum(numbers)
+    });
+  } else if (what === 'multiply') {
+    res.json({
+      "result": arrayMultiply(numbers)
+    });
+  } else if (what === 'double') {
+    res.json({
+      "result": arrayDouble(numbers)
+    });
+  } else {
+    res.json({
+      "error": "Please provide what to do with the numbers!"
+    });
   }
 });
 
