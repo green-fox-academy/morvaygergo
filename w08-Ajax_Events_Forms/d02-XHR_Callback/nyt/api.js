2eae7a024a927225d33ec5645184f408be342f0a
morvaygergo <morvaygergo@gmail.com>
2018-12-30 21:11:26 +0100

    nyt api created

--- w08-Ajax_Events_Forms/d02-XHR_Callback/nyt/api.js
+++ w08-Ajax_Events_Forms/d02-XHR_Callback/nyt/api.js
@@ -1,18 +1,27 @@
-const API_BASE_URL = 'https://api.giphy.com/v1/gifs/search?api_key=XbWuWMZ1MsnVsD4zcsPOVsPSX7b4vUxX&q=funny%20cat&limit=16&offset=0&rating=G&lang=en'
+'use strict';
 
+const API_BASE_URL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=c8fcaefe776f4f4495ff4a216fee2a20&q='Apollo 11'+'landing'";
 const xhr = new XMLHttpRequest();
 
 xhr.onload = () => {
   if (xhr.status === 200) {
     const response = JSON.parse(xhr.responseText);
+    // console.log(response.response.docs[0].headline);
     let articles = document.querySelector('.articles');
-    for (let i = 0; i < response.data.length; i++) {
-      let image = document.createElement('img');
-      image.setAttribute('src', `${response.data[i].images.downsized_still.url}`);
-      image.addEventListener('click', () => {
-        image.setAttribute('src', `${response.data[i].images.downsized.url}`);
-      });
-      images.appendChild(image);
+    for (let i = 0; i < response.response.docs.length; i++) {
+      let element = document.createElement('li');
+      let article = document.createElement('ul');      
+      let headline = document.createElement('li');
+      let snippet = document.createElement('li');
+      let date = document.createElement('li');
+      headline.textContent = response.response.docs[i].headline.main;
+      snippet.textContent = response.response.docs[i].snippet;
+      date.textContent = response.response.docs[i].pub_date;
+      article.appendChild(date);
+      article.appendChild(headline);
+      article.appendChild(snippet);
+      element.appendChild(article);      
+      articles.appendChild(element);      
     }
   }
 };
