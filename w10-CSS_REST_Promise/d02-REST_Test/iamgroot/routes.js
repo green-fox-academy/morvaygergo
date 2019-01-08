5c775b211f1b437e2b4294300c1bc528c1b60a9f
morvaygergo <morvaygergo@gmail.com>
2019-01-08 11:34:17 +0100

    iamgroot test file structure created

---
+++ w10-CSS_REST_Promise/d02-REST_Test/iamgroot/routes.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+
+app.get('/groot', (req, res) => {
+  // TODO: implement this method
+});
+
+module.exports = app;
---
+++ w10-CSS_REST_Promise/d02-REST_Test/iamgroot/test/test-routes.js
@@ -0,0 +1,9 @@
+'use strict';
+​
+const test = require('tape');
+const request = require('supertest');
+const app = require('../routes');
+​
+test('groot endpoint', (t) => {
+  // TODO: implement it 
+});
